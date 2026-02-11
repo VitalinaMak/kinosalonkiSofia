f2dcb340ebd37e659943967d9cad1671f43245da
VitalinaMak <vitamak1108@gmail.com>
2026-02-11 13:14:48 +0200

    Nothing important is changed, just trying to synchronise local files with github

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php 
-    /*$servername = "";
-    $username = "";
-    $password = "";
-    $dbname = "";
+    $servername = "mariadb.rpkk.fi";
+    $username = "sofia";
+    $password = "KinoS2026#?";
+    $dbname = "sofia";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);  //muodostetaan yhteys tietokantaan*/
-
-    require_once 'config/db.php';
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);  //muodostetaan yhteys tietokantaan
+    $conn = new mysqli($servername, $username, $password, $dbname);  //muodostetaan yhteys tietokantaan
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -24,6 +21,10 @@
     <title>Document</title>
 </head>
 <body>
+    <header> 
+        <h1>Kinosalonki Sofia</h1>
+        <a href="login.php">Kirjaudu sisään</a>
+    </header>
     <h1></h1>
     <div class="main"></div>
     <script src="code.js"></script>
