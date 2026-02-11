c564237f0c6ae80ce4345f146eb344820b62d58e
VitalinaMak <vitamak1108@gmail.com>
2026-02-11 14:43:15 +0200

    Basic elements in tapahtumat.php + a little styling for headers

--- tapahtumat.php
+++ tapahtumat.php
@@ -28,6 +28,29 @@
         <h1>Kinosalonki Sofia</h1>
         <a href="login.php">Kirjaudu sisään</a>
     </header>
-    <div class="main"></div>
+    <div class="main">
+        <h1>Tapahtumat</h1>
+        <!-- line with all of the actions for the list -->
+        <div class="eventListActions">
+            <button>Suodata</button>
+            <form action="tapahtumat.php" method="GET"> <!-- search field. When the info is sent, it pass it with URL to the php-function in tapahtumat.php file -->
+                <input type="text" name="search" placeholder="Etsi tapahtumaa">
+                <button type="submit">Etsi</button>
+            </form>
+            <button>Järjestää</button>
+        </div> 
+        <div class="eventList">
+            <!-- list of events (no photos yet, I'll add them a little later) -->
+            <?php 
+                $sql = "SELECT * FROM events;";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<div><h3>".$row['event_name']."</h3><h3>".$row['event_date']."</h3><p>".$row['description']."</p>";
+                    }
+                }
+            ?>
+        </div>
+    </div>
 </body>
 </html>
