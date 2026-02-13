38ddb55f1a6f2222f5f3f3c1b1c8524ebc722a90
VitalinaMak <vitamak1108@gmail.com>
2026-02-13 10:29:44 +0200

    php-code in index and some changes to formatting
    The list of pages is changed into drop-down menu, elements <li> are removed from footer, index now shows actual date and lists all events

--- include/footer.php
+++ include/footer.php
@@ -1,10 +1,8 @@
     <!-- link to bootstrap for using some extratools -->
      <footer class="footer">
         <p>Copyright 2026 Vitalina & Vera & RPKK</p>
-        <ul>
-            <li><a href="https://termify.io/terms-and-conditions-generator?gad_source=1&gad_campaignid=12685596615&gbraid=0AAAAAC6IOXLqKSR22h7VRaB8eMxiR3WLP&gclid=CjwKCAiAkbbMBhB2EiwANbxtbYj6fuC30XVRdrXOfAuqDUNouP0fBWhFRbq--WeacXc02p5xPK6qKhoClDYQAvD_BwE">Terms of Service</a></li>
-            <li><a href="https://termify.io/terms-and-conditions-generator?gad_source=1&gad_campaignid=12685596615&gbraid=0AAAAAC6IOXLqKSR22h7VRaB8eMxiR3WLP&gclid=CjwKCAiAkbbMBhB2EiwANbxtbYj6fuC30XVRdrXOfAuqDUNouP0fBWhFRbq--WeacXc02p5xPK6qKhoClDYQAvD_BwE">Privacy Policy</a></li>
-        </ul>
+        <a href="https://termify.io/terms-and-conditions-generator?gad_source=1&gad_campaignid=12685596615&gbraid=0AAAAAC6IOXLqKSR22h7VRaB8eMxiR3WLP&gclid=CjwKCAiAkbbMBhB2EiwANbxtbYj6fuC30XVRdrXOfAuqDUNouP0fBWhFRbq--WeacXc02p5xPK6qKhoClDYQAvD_BwE">Terms of Service</a>
+        <a href="https://termify.io/terms-and-conditions-generator?gad_source=1&gad_campaignid=12685596615&gbraid=0AAAAAC6IOXLqKSR22h7VRaB8eMxiR3WLP&gclid=CjwKCAiAkbbMBhB2EiwANbxtbYj6fuC30XVRdrXOfAuqDUNouP0fBWhFRbq--WeacXc02p5xPK6qKhoClDYQAvD_BwE">Privacy Policy</a>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
