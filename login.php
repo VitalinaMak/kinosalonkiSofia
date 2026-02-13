cfe64ce00ca0aee919cdca84f814bea2a772e786
VeraRi123 <veramalkova270408@gmail.com>
2026-02-13 11:13:07 +0200

    forms added

--- login.php
+++ login.php
@@ -2,7 +2,22 @@
     $pageTitle = "Login";
     include 'include/header.php'; 
 ?>
-    <h1></h1>
-    <div class="main"></div>
+    <h1>Kirjaudu sisään:</h1>
+
+    
+    <form>
+    <!-- maybe remove the div's ? idk -->
+        <div>
+            <label for="enter_your_email">Sähköpostiosoite:</label>
+            <input type="email" id="enter_your_email" name="enter_your_email" required><br><br>
+        </div>
+        <div>
+            <label for="salasana">Salasana:</label>
+            <input type="password" id="salasana" name="salasana" required><br><br>
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+
+
     
 <?php include 'include/footer.php'; ?>
--- signup.php
+++ signup.php
@@ -3,7 +3,25 @@
     include 'include/header.php'; 
 ?>
     <h1></h1>
-    <div class="main"></div>
+    <div class="main">
+
+        <form>
+            <div>
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" required minlength="2">
+            </div>
+            <div>
+                <label for="email">Sähköpostiosoite:</label>
+                <input type="email" id="email" name="enter_your_email" required>
+            </div>
+            <div>
+                <label for="password">Salasana:</label>
+                <input type="password" id="password" name="password" required minlength="4">
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+
+    </div>
     
 <?php include 'include/footer.php'; ?>
 
