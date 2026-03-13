e5800929d7ce5e22b511a344eb77521f85930c83
VeraRi123 <veramalkova270408@gmail.com>
2026-03-13 14:07:51 +0200

    OOOO IMBA

--- bookEvent.php
+++ bookEvent.php
@@ -1,11 +1,50 @@
 <?php 
     $pageTitle = "BookEvent";
+    $extraCSS = "CSS/book_event.css";
     include 'include/header.php'; 
 ?>
 <main class="bookEvent_page">
 
     <h1></h1>
-    <div class=""></div>
+    <div class="wrapper">
+
+
+    <table><tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody></table>
+
+    </div>
 
 </main>  
 <?php include 'include/footer.php'; ?>
--- editEvent.php
+++ editEvent.php
@@ -48,13 +48,17 @@
                             </div>
                             
                             <div class="fileInputField"> <!-- Фото -->
-                                <label for="eventPicture-input"> Tapahtuman kuva 
+                                <label for="eventPicture-input"> 
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M440-440ZM120-120q-33 0-56.5-23.5T40-200v-480q0-33 23.5-56.5T120-760h126l74-80h240v80H355l-73 80H120v480h640v-360h80v360q0 33-23.5 56.5T760-120H120Zm640-560v-80h-80v-80h80v-80h80v80h80v80h-80v80h-80ZM440-260q75 0 127.5-52.5T620-440q0-75-52.5-127.5T440-620q-75 0-127.5 52.5T260-440q0 75 52.5 127.5T440-260Zm0-80q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29Z"/></svg>
+                                   
+                                    <span class="label-text">Tapahtuman kuva</span> 
+
                                     <input type="file" id="eventPicture-input" name="eventPicture" hidden>  <!-- input-field for the image. It's accessed through the label but it cannot contain information about the previous image-->
                                     <input type="hidden" name="current_image" value="<?= htmlspecialchars($row['event_image']); ?>">  <!-- ..so here is another input (basicaly it keeps track of what the user currently sees) -->
                                     <input type="hidden" name="original_image" value="<?= htmlspecialchars($row['event_image']); ?>"> <!-- and one more hidden input to store the original image from the database -->
                                     <input type="hidden" name="remove_image" value="0">  <!-- a flag that signals if the user clicked “remove image”  -->
                                     <?= "<img id='preview' src='kuvat/tapahtumaKuvat/".$picture."' alt='Uploaded Image'>"; ?>
-                                    <a href="javascript:void(0)" onclick="removeImage()" style="padding: 5px;">Poistaa kuvaa</a>  <!-- a link to remove the picture -->
+                                    <a class="button" href="javascript:void(0)" onclick="removeImage()"> Poistaa kuvaa</a>  <!-- a link to remove the picture -->
                                 </label>
                             </div>
 
@@ -68,8 +72,8 @@
                                     </select>
                                 </div>
                                 <div> <!-- Place -->
-                                    <label for="maxplaces-input">Max. osallistujamäärä: </label> <br>
-                                    <input type="number" id="maxplaces-input" name="maxplaces" value="<?=htmlspecialchars($row['max_visitors']);?>" required>
+                                    <label for="maxplaces-input"> </label> <br>
+                                    <input type="number" id="maxplaces-input" name="maxplaces" placeholder="Max. osallistujamäärä" value="<?=htmlspecialchars($row['max_visitors']);?>" required>
                                 </div>
                             </div>
 
