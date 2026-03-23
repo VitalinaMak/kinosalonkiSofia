aedc7fca1d8b81b8a1528f117ff950bbd2ec30da
VitalinaMak <vitamak1108@gmail.com>
2026-03-23 10:40:51 +0200

    Different display for events in bookEvent.php

--- bookEvent.php
+++ bookEvent.php
@@ -21,13 +21,17 @@
 
     $ageLimit = ($event['age_limit']=="Ei luokiteltu") ? "" : " (".$event['age_limit'].")";  //if the age limit is defined, it appears in parenthesis after the name of the event
 
-    /* retrieve information about booked setas for that event */
+    $eventType = $event['event_type'];  //type of event
+    $maxVisitors = $event['max_visitors'];  //max. amount of visitors for that event
+
+    /* retrieve information about booked seats for that event */
     $stmt = $conn->prepare("SELECT user_id, event_id, seat_number FROM bookings WHERE event_id = ?");
     $stmt->bind_param("s", $eventID);
     $stmt->execute();
     $result = $stmt->get_result();
 
     $bookings = [];  //an array with numbers of all booked seats
+    $bookedSeatsAmount = count($bookings);  //amount of booked seats
     $userAlreadyBooked = 0;  //counter to check if the user has already booked any seats for that event before
 
     while ($row = $result->fetch_assoc()) {
@@ -52,42 +56,48 @@
         <p><?= $event['description'] ?></p>  <!-- Here add all the details about the event - image, decription etc. onclick on place it checks if you're logged in, and if not - sends you to SIGN UP page! -->
 
         <div class="booking">
-            <!-- table for seating chart -->
-            <table><tbody>
-                <caption> näytto </caption>
-                <tr>
-                    <td id="1" <?= in_array(1, $bookings) ? "class='booked'" : "";?> onclick="selection('1')"></td>
-                    <td id="2" <?= in_array(2, $bookings) ? "class='booked'" : "";?> onclick="selection('2')"></td>
-                    <td id="3" <?= in_array(3, $bookings) ? "class='booked'" : "";?> onclick="selection('3')"></td>
-                    <td id="4" <?= in_array(4, $bookings) ? "class='booked'" : "";?> onclick="selection('4')"></td>
-                    <td id="5" <?= in_array(5, $bookings) ? "class='booked'" : "";?> onclick="selection('5')"></td>
-                    <td id="6" <?= in_array(6, $bookings) ? "class='booked'" : "";?> onclick="selection('6')"></td>
-                </tr>
-                <tr>
-                    <td id="7" <?= in_array(7, $bookings) ? "class='booked'" : "";?> onclick="selection('7')"></td>
-                    <td id="8" <?= in_array(8, $bookings) ? "class='booked'" : "";?> onclick="selection('8')"></td>
-                    <td id="9" <?= in_array(9, $bookings) ? "class='booked'" : "";?> onclick="selection('9')"></td>
-                    <td id="10" <?= in_array(10, $bookings) ? "class='booked'" : "";?> onclick="selection('10')"></td>
-                    <td id="11" <?= in_array(11, $bookings) ? "class='booked'" : "";?> onclick="selection('11')"></td>
-                    <td id="12" <?= in_array(12, $bookings) ? "class='booked'" : "";?> onclick="selection('12')"></td>
-                </tr>
-                <tr>
-                    <td id="13" <?= in_array(13, $bookings) ? "class='booked'" : "";?> onclick="selection('13')"></td>
-                    <td id="14" <?= in_array(14, $bookings) ? "class='booked'" : "";?> onclick="selection('14')"></td>
-                    <td id="15" <?= in_array(15, $bookings) ? "class='booked'" : "";?> onclick="selection('15')"></td>
-                    <td id="16" <?= in_array(16, $bookings) ? "class='booked'" : "";?> onclick="selection('16')"></td>
-                    <td id="17" <?= in_array(17, $bookings) ? "class='booked'" : "";?> onclick="selection('17')"></td>
-                    <td id="18" <?= in_array(18, $bookings) ? "class='booked'" : "";?> onclick="selection('18')"></td>
-                </tr>
-                <tr>
-                    <td id="19" <?= in_array(19, $bookings) ? "class='booked'" : "";?> onclick="selection('19')"></td>
-                    <td id="20" <?= in_array(20, $bookings) ? "class='booked'" : "";?> onclick="selection('20')"></td>
-                    <td id="21" <?= in_array(21, $bookings) ? "class='booked'" : "";?> onclick="selection('21')"></td>
-                    <td id="22" <?= in_array(22, $bookings) ? "class='booked'" : "";?> onclick="selection('22')"></td>
-                    <td id="23" <?= in_array(23, $bookings) ? "class='booked'" : "";?> onclick="selection('23')"></td>
-                    <td id="24" <?= in_array(24, $bookings) ? "class='booked'" : "";?> onclick="selection('24')"></td>
-                </tr>
-            </tbody></table>
+            <?php if ($eventType == '1'): ?>
+                <!-- table for seating chart (visible only if event type is 1 (movie) -->
+                <table><tbody>
+                    <caption> näytto </caption>
+                    <tr>
+                        <td id="1" <?= in_array(1, $bookings) ? "class='booked'" : "";?> onclick="selection('1')"></td>
+                        <td id="2" <?= in_array(2, $bookings) ? "class='booked'" : "";?> onclick="selection('2')"></td>
+                        <td id="3" <?= in_array(3, $bookings) ? "class='booked'" : "";?> onclick="selection('3')"></td>
+                        <td id="4" <?= in_array(4, $bookings) ? "class='booked'" : "";?> onclick="selection('4')"></td>
+                        <td id="5" <?= in_array(5, $bookings) ? "class='booked'" : "";?> onclick="selection('5')"></td>
+                        <td id="6" <?= in_array(6, $bookings) ? "class='booked'" : "";?> onclick="selection('6')"></td>
+                    </tr>
+                    <tr>
+                        <td id="7" <?= in_array(7, $bookings) ? "class='booked'" : "";?> onclick="selection('7')"></td>
+                        <td id="8" <?= in_array(8, $bookings) ? "class='booked'" : "";?> onclick="selection('8')"></td>
+                        <td id="9" <?= in_array(9, $bookings) ? "class='booked'" : "";?> onclick="selection('9')"></td>
+                        <td id="10" <?= in_array(10, $bookings) ? "class='booked'" : "";?> onclick="selection('10')"></td>
+                        <td id="11" <?= in_array(11, $bookings) ? "class='booked'" : "";?> onclick="selection('11')"></td>
+                        <td id="12" <?= in_array(12, $bookings) ? "class='booked'" : "";?> onclick="selection('12')"></td>
+                    </tr>
+                    <tr>
+                        <td id="13" <?= in_array(13, $bookings) ? "class='booked'" : "";?> onclick="selection('13')"></td>
+                        <td id="14" <?= in_array(14, $bookings) ? "class='booked'" : "";?> onclick="selection('14')"></td>
+                        <td id="15" <?= in_array(15, $bookings) ? "class='booked'" : "";?> onclick="selection('15')"></td>
+                        <td id="16" <?= in_array(16, $bookings) ? "class='booked'" : "";?> onclick="selection('16')"></td>
+                        <td id="17" <?= in_array(17, $bookings) ? "class='booked'" : "";?> onclick="selection('17')"></td>
+                        <td id="18" <?= in_array(18, $bookings) ? "class='booked'" : "";?> onclick="selection('18')"></td>
+                    </tr>
+                    <tr>
+                        <td id="19" <?= in_array(19, $bookings) ? "class='booked'" : "";?> onclick="selection('19')"></td>
+                        <td id="20" <?= in_array(20, $bookings) ? "class='booked'" : "";?> onclick="selection('20')"></td>
+                        <td id="21" <?= in_array(21, $bookings) ? "class='booked'" : "";?> onclick="selection('21')"></td>
+                        <td id="22" <?= in_array(22, $bookings) ? "class='booked'" : "";?> onclick="selection('22')"></td>
+                        <td id="23" <?= in_array(23, $bookings) ? "class='booked'" : "";?> onclick="selection('23')"></td>
+                        <td id="24" <?= in_array(24, $bookings) ? "class='booked'" : "";?> onclick="selection('24')"></td>
+                    </tr>
+                </tbody></table>
+            <?php elseif ($eventType == '2'): ?>
+                <p>Paikkoja jäljellä: <?=$maxVisitors-$bookedSeatsAmount?></p>  <!-- if event type is 2 (limited amount of places), show the amount of places left -->
+            <?php else: ?>
+                <p>Ilmoittautuneiden määrä: <?=$bookedSeatsAmount?></p>  <!-- if event type is 3 (unlimited amount of places), show the total number of participants-->
+            <?php endif; ?>
 
             <div class="bookingInfo">
                 <p id="showSelectedSeats"></p>
@@ -103,7 +113,7 @@
             </div>
         </div>
 
-        <p>Huom! Yhdellä tunnuksella voi varata enintään 3 paikkaa. Mikäli haluat varata useampia paikkoja, olethan yhteydessä yhdistykseen, jonka kautta se on mahdollista.</p>  <!-- paste here phone number or email, idk, on therir webpage they say they don't accept reservations via email, phone or social media -->
+        <p>Huom! Yhdellä tunnuksella voi varata enintään 2 paikkaa. Mikäli haluat varata useampia paikkoja, olethan yhteydessä yhdistykseen, jonka kautta se on mahdollista.</p>  <!-- paste here phone number or email, idk, on therir webpage they say they don't accept reservations via email, phone or social media -->
 
     </div>
 
