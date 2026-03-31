b1e265423af1d4314bf8d78984a57ad38adbcad3
VitalinaMak <vitamak1108@gmail.com>
2026-03-31 15:18:44 +0300

    Password reset

--- JavaScript/account.js
+++ JavaScript/account.js
@@ -1,5 +1,15 @@
 "use strict";
 
+//turned out it integrated in html, so there's no real need in this function
+/* const passwordInput = document.getElementById("oldPassword-input");
+passwordInput.addEventListener("click", function() {
+  if (this.getAttribute('type') == 'password') {
+    this.setAttribute('type', 'text');
+  } else {
+    this.setAttribute('type', 'password');    
+  }
+})  */   
+
 function toggleEdit() {
   // Находим все инпуты в форме
   const inputs = document.querySelectorAll("#profile input");
@@ -12,8 +22,6 @@ function toggleEdit() {
       // Включаем режим редактирования
       input.removeAttribute("readonly");
       input.classList.add("activated");  //add classname for styling
-      /* input.style.backgroundColor = "var(--cinema-whiteish)"; // Делаем фон светлее, чтобы было видно, что можно писать
-      input.style.border = "3px solid var(--blue)"; */
     }
   });
 
@@ -24,4 +32,20 @@ function toggleEdit() {
   // Скрываем кнопку "Редактировать" и показываем "Сохранить"
   editBtn.style.display = "none";
   saveBtn.style.display = "inline-block";
+}
+
+/* function to display fields for password eset and remove fields with personal information */
+function changePasswordForm() {
+  const personalInfo = document.getElementsByClassName("personalInfo");
+  const passwordReset = document.getElementsByClassName("passwordReset");
+
+  //hide inputs for personal info
+  for (let i = 0; i < personalInfo.length; i++) {
+    personalInfo[i].style.display = "none";
+  }
+
+  //reveal inputs for password reset
+  for (let i = 0; i < passwordReset.length; i++) {
+    passwordReset[i].style.display = "flex";
+  }
 }
--- changeInfo.php
+++ changeInfo.php
@@ -25,38 +25,88 @@
     $stmt->close();
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        /* assign values to variables */
-        $username = trim($_POST['name']);
-        $email = trim($_POST['email']);
 
-        // Check if new email already exists
-        if ($email != $oldEmail) {
-            $checkEmailStmt = $conn->prepare("SELECT email FROM users WHERE email = ?");
-            $checkEmailStmt->bind_param("s", $email);
-            $checkEmailStmt->execute();
-            $checkEmailStmt->store_result();
-            if ($checkEmailStmt->num_rows > 0) {
-                echo "Sähköposti on jo käytössä.";
+        /* functionality for password reset */
+        if (isset($_POST['newPassword'])) {
+            /* retrieve the old password hash from database */
+            $stmt = $conn->prepare("SELECT password_hash FROM users WHERE id = ?");
+            $stmt->bind_param("s", $userID);
+            $stmt->execute();
+            $stmt->store_result();
+            // bind result to variable
+            $stmt->bind_result($passHashFromDB);
+            // fetch the result into the variable
+            $stmt->fetch();
+            $stmt->close();
+
+            /* assign new values to the variables */
+            $oldPass = $_POST['oldPassword'];
+            $newPass = $_POST['newPassword'];
+            $newPassRepeat = $_POST['newPasswordRepeat'];
+            
+           if ($newPass != $newPassRepeat) {
+                die ('Uusi salasanat eivät täsmää!');
             }
-        }
 
-        /* UPDATE INFORMATION */
+            /* check if the old password is valid */
+            if (password_verify($oldPass, $passHashFromDB)) {
+                echo "Password is verified";
+                $updateStmt = $conn->prepare("UPDATE users SET password_hash=? WHERE id=?;");  // Prepare and bind parameters to sql-query
         
-        $updateStmt = $conn->prepare("UPDATE users SET email=?, username=? WHERE id=?;");  // Prepare and bind parameters to sql-query
-    
-        if (!$stmt) {
-            die("Prepare failed: " . $conn->error);
-        }
-        $updateStmt->bind_param("sss", $email, $username, $userID);
+                if (!$stmt) {
+                    die("Prepare failed: " . $conn->error);
+                }
 
-        if ($updateStmt->execute()) {
-            /* echo "Tiedot on päivitetty onnistuneesti"; */
-            $_SESSION['success_message'] = "Tiedot on päivitetty onnistuneesti";  //save the message into session
-            header("Location: account.php");  //redirect back to account.php
+                $newHash = password_hash($newPass, PASSWORD_DEFAULT);  //hashed new password from input
+
+                $updateStmt->bind_param("ss", $newHash, $userID);
+        
+                if ($updateStmt->execute()) {
+                    /* echo "Tiedot on päivitetty onnistuneesti"; */
+                    $_SESSION['success_message'] = "Tiedot on päivitetty onnistuneesti";  //save the message into session
+                    header("Location: account.php");  //redirect back to account.php
+                } else {
+                    echo "Error: " . $stmt->error;
+                }
+
+            } else {
+                echo "Vanha salasana on virheellinen!";
+            }
+
         } else {
-            echo "Error: " . $stmt->error;
-        }
+            /* assign values to variables */
+            $username = trim($_POST['name']);
+            $email = trim($_POST['email']);
+    
+            // Check if new email already exists
+            if ($email != $oldEmail) {
+                $checkEmailStmt = $conn->prepare("SELECT email FROM users WHERE email = ?");
+                $checkEmailStmt->bind_param("s", $email);
+                $checkEmailStmt->execute();
+                $checkEmailStmt->store_result();
+                if ($checkEmailStmt->num_rows > 0) {
+                    echo "Sähköposti on jo käytössä.";
+                }
+            }
+    
+            /* UPDATE INFORMATION */
+            
+            $updateStmt = $conn->prepare("UPDATE users SET email=?, username=? WHERE id=?;");  // Prepare and bind parameters to sql-query
         
+            if (!$stmt) {
+                die("Prepare failed: " . $conn->error);
+            }
+            $updateStmt->bind_param("sss", $email, $username, $userID);
+    
+            if ($updateStmt->execute()) {
+                /* echo "Tiedot on päivitetty onnistuneesti"; */
+                $_SESSION['success_message'] = "Tiedot on päivitetty onnistuneesti";  //save the message into session
+                header("Location: account.php");  //redirect back to account.php
+            } else {
+                echo "Error: " . $stmt->error;
+            }
+            
+        }
         
     }
 ?>
