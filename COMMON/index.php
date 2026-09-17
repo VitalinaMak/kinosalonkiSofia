8de0a8e5e1c736f63e0aae517baa56899d3462dd
VeraRi123 <veramalkova270408@gmail.com>
2026-09-17 15:36:40 +0300

    index designing; empty menu page created

--- COMMON/index.php
+++ COMMON/index.php
@@ -6,18 +6,17 @@
 
 
 <main class="index_page"> 
-      <div class="about">
-          <!-- Pictures and "history" here -->
-<!--           
-          <div class="photos">
-            <img src="kuvat/interior.jpg" alt="kinosalonki sisällä">
-            <img src="kuvat/building.jpg" alt="piha">
-            <img src="kuvat/posters.webp" alt="kuvat">
-          </div> -->
-          
-          <p>Vuonna 1844 Sofia Lybecker perustui köyhille tytöille ja orpolapsille tarkoitettu Lybeckerin tyttökoulu hänen äitinsä perintörahoilla. Koulu toimi aluksi ilman omaa tilaa, mutta vuonna 1859 Sofian sisko Helene Bergbom ja hänen miehensä Carl Gustaf lahjoittivat koululle omistamansa talon nykyiseltä Reiponkadulta. Koulu toimi tässä talossa seuraavat 125 vuotta. Entistä koulurakennusta kutsutaan nykyään Sofian taloksi ja siinä toimii Kinosalonki Sofia. Me jatkamme Sofian talon kasvatustyötä yhteisöpedagogisissa kulttuurihankkeissamme sekä järjestämällä ja tukemalla elokuva- ja mediakasvatusta.</p>    
+      <div class="about history" style="grid-area: historyarea">
+        <p>Vuonna 1844 Sofia Lybecker perustui köyhille tytöille ja orpolapsille tarkoitettu Lybeckerin tyttökoulu hänen äitinsä perintörahoilla. Koulu toimi aluksi ilman omaa tilaa, mutta vuonna 1859 Sofian sisko Helene Bergbom ja hänen miehensä Carl Gustaf lahjoittivat koululle omistamansa talon nykyiseltä Reiponkadulta. Koulu toimi tässä talossa seuraavat 125 vuotta. Entistä koulurakennusta kutsutaan nykyään Sofian taloksi ja siinä toimii Kinosalonki Sofia. Me jatkamme Sofian talon kasvatustyötä yhteisöpedagogisissa kulttuurihankkeissamme sekä järjestämällä ja tukemalla elokuva- ja mediakasvatusta.</p> 
+        <a href="../USER/tapahtumat.php" class="button">Katso kaikki tapahtumat</a>  
       </div>
-          
+
+      <div class="about menu" style="grid-area: menuarea">
+        <p>Our menu lalala Rich espresso swirled with velvety steamed milk and a touch of house-made vanilla bean syrup. It tastes like a warm blanket on a chilly morning. Dark, decadent chocolate melted into a double shot of espresso, topped with whipped cream and cocoa dust. </p>    
+        <a href="menu.php" class="button">Go to menu page</a>  
+      </div>
+
+      <div class="about today" style="grid-area: todayarea">
       <?php 
         $date = date('d. F Y');  //gets current date
         echo "<h2> Tapahtumat tänään, $date:</h2>";
@@ -25,19 +24,20 @@
       ?>
       
       <div class="eventList">
-                <?php
-                /* printing out the date, time and the name of event (test) */
-                  
-                include '../MODULES/events_query.php';  //base query for events (NOTE: it's not completed here!). It uses variables $sql, $params[]
-                
-                $sql .= " WHERE events.event_date = :date GROUP BY events.id ORDER BY events.event_time;";  //end of the query
-                
-                $params[':date'] = $date;
-                
-                include "../MODULES/events_display.php";  //display events. It uses variables $sql, $params, $hasRows, $row, $bgColor, $typeForColor, $kuvaPath, $placesNumber, $ageLimit, $imageHtml 
-                ?>
-        </div>   
-        <a href="../USER/tapahtumat.php" class="btn btn-outline-danger">Katso kaikki tapahtumat</a>  
+        <?php
+        /* printing out the date, time and the name of event (test) */
+          
+        include '../MODULES/events_query.php';  //base query for events (NOTE: it's not completed here!). It uses variables $sql, $params[]
+        
+        $sql .= " WHERE events.event_date = :date GROUP BY events.id ORDER BY events.event_time;";  //end of the query
+        
+        $params[':date'] = $date;
+        
+        include "../MODULES/events_display.php";  //display events. It uses variables $sql, $params, $hasRows, $row, $bgColor, $typeForColor, $kuvaPath, $placesNumber, $ageLimit, $imageHtml 
+        ?>
+      </div> 
+      </div>
+  
     </main>
 
 <?php include '../include/footer.php'; ?>
